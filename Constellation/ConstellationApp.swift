bb12fe4e77c502a5e2c24a8ca65463b7c1814a2f
Aydar N. (Sublime Merge MBP) <me@aydar.media>
2023-07-22 12:59:34 +0300

    Initial Commit

---
+++ Constellation/ConstellationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConstellationApp.swift
+//  Constellation
+//
+//  Created by aydar.media on 22.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ConstellationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
